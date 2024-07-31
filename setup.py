dd32b3e0575b3f27699b493732cce624553aef79
DYeag <37943859+DYeag@users.noreply.github.com>
2024-07-31 13:29:35 -0700

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Partial SDK Coverage for PokeAPI'
 LONG_DESCRIPTION = 'Not to be used in production'
 
